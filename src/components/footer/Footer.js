78a20e65d7e235993a6e3a3b923a2d86f45aec1a
supercode01 <annaelise032@gmail.com>
2025-09-07 22:18:40 +0500

    add links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -28,25 +28,25 @@ const Footer = () => {
         <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              <a href="" target="_blank" rel="noopener noreferrer">About Me</a>
+              <a href="" target="" rel="noopener noreferrer">About Me</a>
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              <a href="" target="_blank" rel="noopener noreferrer">Services</a>
+              <a href="" target="" rel="noopener noreferrer">Services</a>
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              <a href="#portfolio" target="_blank" rel="noopener noreferrer">Portfolio</a>
+              <a href="#" target="" rel="noopener noreferrer">Portfolio</a>
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              <a href="#contact" target="_blank" rel="noopener noreferrer">Contact</a>
+              <a href="#contact" target="" rel="noopener noreferrer">Contact</a>
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
